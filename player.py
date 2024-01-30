4baf207d38739241807c4a57df2de8f380c4fb84
jcarbolynn <36271436+jcarbolynn@users.noreply.github.com>
2024-01-30 16:58:39 -0500

    Update player.py
    older version no set up

--- player.py
+++ player.py
@@ -2,19 +2,19 @@ class Player:
   def __init__(self, name):
     self.name = name
     self.inventory = []
-    self.health = 100.0
+    self.health = 10.0
 
-  def add_inventory(self, item, current_room):
+  def add_inventory(self, items, current_room):
     '''adds item to player inventory, pops from room inventory'''
-    self.inventory.append(item)
-    current_room.remove_item(item)
-    print(f"{item.name} was taken")
+    for item in items:
+      if not item.fixed:
+        # self.inventory = self.inventory.append(current_room.items.pop(item))
+        print(f"{item.name} is {item.fixed}")
+        self.inventory.append(item)
+        current_room.items.remove(item)
+        print(f"{item.name} was taken")
 
-  def remove_inventory(self, item, current_room):
-    '''adds item to player inventory, pops from room inventory'''
-    self.inventory.remove(item)
-    current_room.add_item(item)
-    print(f"Placed {item.name} in {current_room.name}")
+  #  WHY IS IT NOT GOING THROUGH FOR LOOP AGAIN
 
   def display_inventory(self):
     for item in self.inventory:
