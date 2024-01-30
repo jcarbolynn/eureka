a32d27bcbc5792632e1c4615417074247dcdb717
jcarbolynn <36271436+jcarbolynn@users.noreply.github.com>
2024-01-30 16:57:44 -0500

    Update item.py
    older version no set up

--- item.py
+++ item.py
@@ -1,15 +1,8 @@
 class Item:
-  def __init__(self, name, description):
-    self.name = name
-    self.description = description
-    # self.fixed = False
-    # self.moved = False
-    self.wearable = False
-    self.concealed = False
-
-  # def __init__(self, name, description, fixed):
-  #   self.name = name
-  #   self.description = description
-  #   self.fixed = fixed
-  #   self.wearable = False
-  #   self.concealed = False
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.fixed = False
+        self.moved = False
+        self.wearable = False
+        self.concealed = False
